58038421097dd3f9011420489e9412e863b011d5
Mike Butler <michael.butler@progress.com>
2023-05-05 09:30:50 -0400

    add some ChefSpec. It mostly works if you remove audit

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,14 +7,15 @@
 require 'chefspec'
 
 describe 'win_choco::default' do
-  context 'When all attributes are default, on Windows 2016' do
+  context 'When all attributes are default, on Windows 2019' do
     let(:chef_run) do
-      runner = ChefSpec::ServerRunner.new(platform: 'windows', version: '2016')
+      runner = ChefSpec::SoloRunner.new(platform: 'windows', version: '2019')
+      runner.node.normal['tags'] = %w(chef_developer choco_openjdk)
+      runner.default['audit']['compliance_phase'] = true
       runner.converge(described_recipe)
     end
-
     it 'converges successfully' do
-      expect { chef_run }.to raise_error
+      expect { chef_run }.to_not raise_error
     end
   end
 end
